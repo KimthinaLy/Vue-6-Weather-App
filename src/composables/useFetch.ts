93907a2d694aecb47329700e1ba648b3e56391f5
Kimthina Ly <kimthinaly7@gmail.com>
2026-06-15 15:50:22 +0700

    feat: update useFetch to handle bad http request case

--- src/composables/useFetch.ts
+++ src/composables/useFetch.ts
@@ -11,8 +11,11 @@ export function useFetch<T>(url: string) {
       const response = await fetch(url)
       if (response.ok) {
         data.value = await response.json()
+      } else {
+        error.value = `Error: ${response.status} ${response.statusText}`
       }
     } catch (err) {
+      //only come to fetch, if there's a network failure — like no internet, or the URL is completely unreachable.
       error.value = err instanceof Error ? err.message : 'Unknown error'
     } finally {
       loading.value = false
